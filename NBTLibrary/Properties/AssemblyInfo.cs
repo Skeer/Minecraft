696b9fe57dbca42d8e78f1342f0e474125f32d64
Eric Chu <magicskeer@hotmail.com>
2011-02-13 12:12:53 -0800

    Modified NBTLibrary's Assembly Information.

--- NBTLibrary/Properties/AssemblyInfo.cs
+++ NBTLibrary/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NBTLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NBTLibrary")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Eric Chu 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
