1a0e66300da009985fdbbbbb9e0758bb77c81d43
Eric Chu <magicskeer@hotmail.com>
2011-02-14 19:31:16 -0800

    Completed saving Player data.

---
+++ Minecraft/Items/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace Minecraft.Items
+{
+    public class Item // Might need rename to InventoryItem
+    {
+        public short ID { get; set; }
+        public short Damage { get; set; } // Maybe a default?
+        public byte Count { get; set; }
+        public byte Slot { get; set; }
+    }
+}
